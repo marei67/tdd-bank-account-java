18b7df4fdefe1e23e05a16b7744481ae1033fd55
marei67 <68219143+marei67@users.noreply.github.com>
2020-07-16 16:37:26 +0200

    Test with simple withdrawn

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void newAccountShouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+   @Test
+    public void depositMulipleAmountsToIcreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
+
+    @Test
+    public void withdrawSingleAmountToDecreaseTheBalance() {
+        Account account = new Account();
+        account.withdraw( 10);
+        assertThat(account.balance()).isEqualTo(-10);
     }
 }
