1b175473746214176344a6025d66545916a552f5
marei67 <68219143+marei67@users.noreply.github.com>
2020-07-16 16:25:00 +0200

    manual try

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,20 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance = 0;
+
+    public int balance() {
+        return balance;
+    }
+
+    /*
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+     */
 }
